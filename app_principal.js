6e96d4e0e8bfd481ef25ebed1a00104c9c429d67
juanmager <juanmadaria@hotmail.es>
2020-06-02 12:02:13 -0300

    Update checkbox

--- app_principal.js
+++ app_principal.js
@@ -9,17 +9,20 @@ document.addEventListener('DOMContentLoaded', function (event) {
     var solicitudes = [{
             "Fecha Solicitud": "30/03/2020",
             "Descripción": "Pedido de notebook",
-            "Estado": "Abierta"
+            "Estado": "Abierta",
+            "Seleccionar": " "
         },
         {
             "Fecha Solicitud": "03/04/2020",
             "Descripción": "Cambio de horario",
-            "Estado": "En progreso"
+            "Estado": "En progreso",
+            "Seleccionar": " "
         },
         {
             "Fecha Solicitud": "12/05/2020",
             "Descripción": "Pedido de una bicicleta para llegar a la universidad",
-            "Estado": "Cerrada"
+            "Estado": "Cerrada",
+            "Seleccionar": " ",
         }
     ]
 
@@ -47,11 +50,27 @@ document.addEventListener('DOMContentLoaded', function (event) {
 
     for (var i = 0; i < solicitudes.length; i++) {
         tr = table.insertRow(-1);
-        var checkbox = document.createElement("INPUT"); //Added for checkbox
-        checkbox.type = "checkbox"; //Added for checkbox
         for (var j = 0; j < col.length; j++) {
             var tabCell = tr.insertCell(-1);
             tabCell.innerText = solicitudes[i][col[j]];
+
+            if (j == 3) {
+
+                var checkbox = document.createElement("input"); //Added for checkbox
+                checkbox.type = "checkbox"; //Added for checkbox
+                tabCell.appendChild(checkbox);
+            }
+        }
+
+        function check() {
+            document.getElementById(checkbox).checked = true;
+            
+
         }
     }
+
+    function uncheck() {
+        document.getElementById(checkbox).checked = false;
+    }
+
 });
