409d63dd8872dfbad496390f4e3fd33e38c3d037
juanmager <juanmadaria@hotmail.es>
2020-06-14 20:00:40 -0300

    Upload ABM Users

---
+++ app_abm_user.js
@@ -0,0 +1,31 @@
+var exit = document.getElementById("cerrar-sesion");
+exit.addEventListener('click', () => {
+    document.location.href = "index.html"
+})
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    } 
+}
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
