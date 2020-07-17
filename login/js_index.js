45f5bf2d9b2c4f6aa709f98725aef567c5c63651
Pepitalapistolera <acrodri991@gmail.com>
2020-07-16 22:05:15 -0300

    ultimos cambios

--- login/js_index.js
+++ login/js_index.js
@@ -2,50 +2,45 @@ var boton = document.getElementById('botonIngresar')
 var error = document.getElementById('error')
 var crearUsuarioBtn = document.getElementById('crear-usuario')
 
-boton.addEventListener('click', function (event) {
-        usuario = document.getElementById("usuario").value;
-        contraseña = document.getElementById("contraseña").value
-    
-    
-        if(validacionUsuario(usuario) && validacionContraseña(contraseña)){
-            
-            location.href="../principal/pagina_principal.html"
-            
-            /*window.location.href = 'principal.html';*/
-        }
-        else{
-            document.getElementById("error").style.display = "block"
-            error.innerHTML = "Los datos ingresados no son correctos"
-        }
-    }
-  )
+boton.addEventListener('click', function(event) {
+    usuario = document.getElementById("usuario").value;
+    contraseña = document.getElementById("contraseña").value
 
-function validacionUsuario(usuario){
+
+    if (validacionUsuario(usuario) && validacionContraseña(contraseña)) {
+
+        location.href = "./principal/pagina_principal.html"
+
+        /*window.location.href = 'principal.html';*/
+    } else {
+        document.getElementById("error").style.display = "block"
+        error.innerHTML = "Los datos ingresados no son correctos"
+    }
+})
+
+function validacionUsuario(usuario) {
     return /^\d{8}$/.test(usuario)
 }
 
-function validacionContraseña(contraseña){
+function validacionContraseña(contraseña) {
 
     tieneLetra = false
     tieneNumero = false
     caracterIncorrecto = false
 
-    if(contraseña.length >= 6){
-        for(var i = 0; i < contraseña.length; i++){
+    if (contraseña.length >= 6) {
+        for (var i = 0; i < contraseña.length; i++) {
 
             valorCaracter = contraseña.charCodeAt(i)
-            
-            if((valorCaracter >= 65 && valorCaracter <= 90) || (valorCaracter >= 97 && valorCaracter <= 122)){
-                
-                tieneLetra = true;   
-            }
 
-            else if(contraseña.charCodeAt(i) >= 48 && contraseña.charCodeAt(i) <= 57)
+            if ((valorCaracter >= 65 && valorCaracter <= 90) || (valorCaracter >= 97 && valorCaracter <= 122)) {
 
-			{
-				tieneNumero = true;
-            }
-            else{
+                tieneLetra = true;
+            } else if (contraseña.charCodeAt(i) >= 48 && contraseña.charCodeAt(i) <= 57)
+
+            {
+                tieneNumero = true;
+            } else {
 
                 caracterIncorrecto = true;
             }
@@ -56,7 +51,7 @@ function validacionContraseña(contraseña){
 
 }
 
-crearUsuarioBtn.addEventListener('click', ()=> {
+crearUsuarioBtn.addEventListener('click', () => {
     localStorage.setItem('esNuevo', true)
-    document.location.href = "../usuarios/cm_usuario.html"
+    document.location.href = "./usuarios/cm_usuario.html"
 })
